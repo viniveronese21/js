82b5b7f74683f92be87141fadd948c57401e61f5
viniveronese21 <vinicius21veronese@gmail.com>
2022-06-03 13:39:45 -0300

    study

--- 1-pilotando-a-DOM/aula6-criando-e-adicionando-elementos/script.js
+++ 1-pilotando-a-DOM/aula6-criando-e-adicionando-elementos/script.js
@@ -1,6 +1,27 @@
 // Criando e Adicionando elementos 
 
-// createElement
+//createElement
 const div = document.createElement('div');
+div.innerText = "Olá Devs!"
 
-// append preppend
+
+//insertBefore
+
+const body = document.querySelector('body')
+const script = body.querySelector('script')
+// const header = body.querySelector('header') simulando insertAfter pois não existe 
+// body.insertBefore(div, header.nextSibling)
+body.insertBefore(div, script)
+
+/*
+append prepend
+
+const body = document.querySelector('body')
+
+body.prepend(div)
+*/
+
+/*
+    insertBfore
+
+*/
---
+++ 1-pilotando-a-DOM/aula7-eventos/script.js
@@ -0,0 +1,23 @@
+// Eventos
+
+const input = document.querySelector('h1')
+
+const h1 = document.querySelector('h1')
+
+h1.addEventListener('click', print)
+
+print = () => {
+    console.log('print')
+}
+
+
+
+
+
+
+
+
+// onKeyDown, onKeyPress onKeyUp
+// input.onkeypress = function () {
+//     console.log('rodei')
+// }
